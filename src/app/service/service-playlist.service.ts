8061753c4a1166fe167b4f84b798ffd4e56bb014
Yorick <yorick-1996@hotmail.com>
2017-11-22 10:37:57 +0100

    playlist

--- src/app/service/service-playlist.service.ts
+++ src/app/service/service-playlist.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
 import {Playlist} from "../models/playlist";
 import {Observable} from "rxjs/Observable";
+import {HttpParams} from "@angular/common/http";
 
 @Injectable()
 export class ServicePlaylistService {
 
   constructor(public http: Http) { }
 
-  public createMusique(playlist: Playlist): Observable<Playlist> {
+  public getPlaylist(mail: string): Observable<Playlist[]> {
+    return this.http.get("http://localhost:61812/api/playlist").map(response => response.json(), {
+      params:new HttpParams().set("mail",mail+"").toString()
+    });
+  }
+
+  public createPlaylist(playlist: Playlist): Observable<Playlist> {
     return this.http.post('http://localhost:61812/api/playlist', playlist.getCleanDataForSending()).map(response => response.json());
   }
 
-  public updateMusique(playlist: Playlist): Observable<any> {
+  public updatePlaylist(playlist: Playlist): Observable<any> {
     return this.http.put('http://localhost:61812/api/playlist', playlist.getCleanDataForSendingUpdate());
   }
 }
