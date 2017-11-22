457e68afcf2f07fe2c54234d2dc7f5dfde0a38bc
Yorick <yorick-1996@hotmail.com>
2017-11-22 08:57:57 +0100

    ok

--- src/app/service/service-musique.service.spec.ts
+++ src/app/service/service-musique.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { ServiceMusiqueService } from './service-musique.service';
+import { MusiqueService } from './service-musique.service';
 
 describe('ServiceMusiqueService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ServiceMusiqueService]
+      providers: [MusiqueService]
     });
   });
 
-  it('should be created', inject([ServiceMusiqueService], (service: ServiceMusiqueService) => {
+  it('should be created', inject([MusiqueService], (service: MusiqueService) => {
     expect(service).toBeTruthy();
   }));
 });
