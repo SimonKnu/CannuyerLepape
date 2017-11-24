4f7cdbc661d92e03fa0b7edaa955d20ca7a1689b
SimonKnu <anderlecht.11@hotmail.com>
2017-11-24 16:17:04 +0100

    connexion ok

--- src/app/service/service-connexion.service.ts
+++ src/app/service/service-connexion.service.ts
@@ -3,22 +3,19 @@ import {Headers, Http, RequestOptions} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import 'rxjs/add/operator/map';
 import {Membre} from "../models/membre";
-import {HttpParams} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Injectable()
 export class ConnexionService {
 
-  constructor(public http:Http) { }
+  constructor(public http:HttpClient) { }
 
   public getConnexion(mail:string, mot_de_passe:string):Observable<string>{
-    return this.http.get("http://localhost:61812/api/token",{
-      params:new HttpParams().set("mail",mail).set("mot_de_passe",mot_de_passe).toString()
-    }).map(res=>res.json());
+    return this.http.get<string>("http://localhost:61812/api/token?mail="+mail+"&mot_de_passe="+mot_de_passe);
   }
 
   public getMembre(mail:string):Observable<Membre>{
-    let headers = new Headers({ 'Authorization': 'Bearer ' + localStorage.getItem("tokenStorage")});
-    let options = new RequestOptions({ headers: headers });
-    return this.http.get("http://localhost:61812/api/Membre?mail="+mail,options).map(res=>res.json());
+    let headers = new HttpHeaders().set('Authorization','Bearer ' + localStorage.getItem("tokenStorage"));
+    return this.http.get("http://localhost:61812/api/Membre?mail="+mail,{headers:headers});
   }
 }
