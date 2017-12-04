ef5a9ab1cbd173ee5aaefa03cff082254ec21d60
Yorick <yorick-1996@hotmail.com>
2017-12-04 20:24:28 +0100

    playlist ok

--- src/app/gestion-playlist/gestion-playlist.component.ts
+++ src/app/gestion-playlist/gestion-playlist.component.ts
@@ -16,6 +16,7 @@ export class GestionPlaylistComponent implements OnInit {
   private listePlaylist: Playlist[] = [];
   private listeMusique: Musique[] = [];
   private listAdd: Musique[] = [];
+  private listeTmp: Musique[] = [];
   private nomPlaylist = '';
   private id_playlist = 0;
   private isCollapsed = true;
@@ -37,11 +38,27 @@ export class GestionPlaylistComponent implements OnInit {
   }
 
   public updateList(playlist: Playlist, index: number) {
+
     this.index = index;
     this.nomPlaylist = playlist.nom;
     this.id_playlist = playlist.id_playlist;
     this.musiqueService.getMusique(this.id_playlist).subscribe(listeMusique => {
       this.listeMusique = Musique.fromJSONs(listeMusique);
+
+      this.listeTmp = [];
+      let tmp = 0;
+      for (let i = 0; i < this.listAdd.length; i++){
+        for (let j = 0; j < this.listeMusique.length; j++){
+          if (this.listAdd[i].id_musique == this.listeMusique[j].id_musique) {
+            tmp = 1;
+          }
+        }
+        if (tmp == 0) {
+          this.listeTmp.push(this.listAdd[i]);
+        }else{
+          tmp = 0;
+        }
+      }
     });
   }
 
@@ -53,17 +70,18 @@ export class GestionPlaylistComponent implements OnInit {
   }
 
   public delete(id_musique: number, index: number) {
+    this.listeTmp.push(this.listeMusique[index]);
+
     const DELETE = () => this.listeMusique.splice(index, 1);
     const DISPLAY_ERROR = (error) => console.error(error);
 
     this.playlistmusiqueService.deleteMusique(id_musique, this.id_playlist).subscribe(DELETE, DISPLAY_ERROR);
   }
 
-  public createPlaylistMusique(musique: Musique) {
-    if (this.listeMusique.indexOf(musique) === -1) {
-      this.listeMusique.push(musique);
-      this.playlistmusiqueService.createPlaylistMusique(new Playlistmusique(musique.id_musique, this.id_playlist)).subscribe();
-    }
+  public createPlaylistMusique(musique: Musique, index: number) {
+    this.listeTmp.splice(index, 1);
+    this.listeMusique.push(musique);
+    this.playlistmusiqueService.createPlaylistMusique(new Playlistmusique(musique.id_musique, this.id_playlist)).subscribe();
     this.estCache = true;
   }
 
